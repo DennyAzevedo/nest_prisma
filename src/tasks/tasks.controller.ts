723cb46ef452d2a81914a4f8524fb061623d074f
Denny Azevedo <denny_af@yahoo.com.br>
2026-03-31 21:06:13 -0300

    Interceptors para log de requisições, log do corpo da requisição e adição de cabeçalho personalizado. Implementação de paginação na listagem de tarefas.

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -7,18 +7,28 @@ import {
 	Body,
 	Put,
 	Delete,
-	ParseIntPipe
+	ParseIntPipe,
+	UseInterceptors
 } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { UpdateTaskDto } from './dto/update.task.dto';
 import { CreateTaskDto } from './dto/create.task.dto';
 import { PaginationDto } from '../common/dto/pagination.dto';
+import { LoggerInterceptor } from '../common/interceptors/logger.interceptor';
+import { BodyCreateTaskInterceptor } from 'src/common/interceptors/body-create-task.interceptor';
+import { AddHeaderInterceptor } from 'src/common/interceptors/add-header.interceptor';
 
 @Controller('tasks')
+	//@UseInterceptors(LoggerInterceptor)
+	// Podemos usar o interceptor para todas as rotas do controller, ou apenas para rotas específicas,
+	// como a rota de listagem de tarefas, por exemplo.
+	// Depois de testado, podemos decidir onde aplicar o interceptor.
 export class TasksController {
 	constructor(private readonly taskService: TasksService) {}
 
 	@Get()
+	@UseInterceptors(LoggerInterceptor)
+	@UseInterceptors(AddHeaderInterceptor)
 	// Depois de testado no Postmamp, podemos definir um DTO para os parâmetros de consulta
 	//findAllTasks(@Query() params: any) {
 	findAllTasks(@Query() paginationDto: PaginationDto) {
@@ -33,6 +43,7 @@ export class TasksController {
 	}
 
 	@Post()
+	@UseInterceptors(BodyCreateTaskInterceptor)
 	createTask(@Body() createTaskDto: CreateTaskDto) {
 		return this.taskService.create(createTaskDto)
 	}
